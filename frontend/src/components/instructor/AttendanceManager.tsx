9b8bdbb8da0bb545c0b167ae9f7afaebff9feb3e
PrashannaChauhanKshetri <normieprashanna@gmail.com>
2026-03-14 21:39:44 +0545

    fix: include initialSectionId in attendance effect deps

--- frontend/src/components/instructor/AttendanceManager.tsx
+++ frontend/src/components/instructor/AttendanceManager.tsx
@@ -72,7 +72,7 @@ export default function AttendanceManager({ sectionId: initialSectionId }: { sec
     };
 
     fetchUnits();
-  }, []);
+  }, [initialSectionId]);
 
   // Fetch enrolled students and attendance when unit or date changes
   useEffect(() => {
